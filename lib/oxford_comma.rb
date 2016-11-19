56b7654b879b3107fdeb09f01b2b50ad673d668f
Christina Entcheva <christina.entcheva@gmail.com>
2016-11-19 22:01:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    final = ""
+    for idx in 1..array.length - 1
+      final.concat(array[idx - 1] + ", ")
+    end
+    final.concat("and " + array[-1])
+    final
+  end
 end
+
+
+# array.length == 3
+#   return array.insert(-2, "and").join(", ")
+#   return
